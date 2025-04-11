6736354f6f4c0bc43d8e9684c1ba55e8c39791f6
William H <william.hermozo@protonmail.com>
2025-04-11 16:56:16 +0200

    config

--- lib/web3.ts
+++ lib/web3.ts
@@ -1,21 +1,23 @@
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultConfig as getRainbowConfig } from '@rainbow-me/rainbowkit';
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { shape, shapeSepolia } from 'viem/chains';
 import { http } from 'viem';
 
-export const rainbowConfig = getRainbowConfig({
+const transports = {
+  [shape.id]: http(
+    `https://shape-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
+    { batch: true }
+  ),
+  [shapeSepolia.id]: http(
+    `https://shape-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
+    { batch: true }
+  ),
+};
+
+export const config = getDefaultConfig({
   appName: 'Assembly',
   projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
   chains: [shape, shapeSepolia],
-  transports: {
-    [shape.id]: http(
-      `https://shape-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
-      { batch: true }
-    ),
-    [shapeSepolia.id]: http(
-      `https://shape-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
-      { batch: true }
-    ),
-  },
+  transports,
   ssr: true,
 });
