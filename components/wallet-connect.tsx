7d819046cf65aa69d7cc3bf931aa1ebec241e803
William H <william.hermozo@protonmail.com>
2025-04-11 17:10:10 +0200

    style connect wallet

--- components/wallet-connect.tsx
+++ components/wallet-connect.tsx
@@ -1,7 +1,35 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import { abbreviateHash } from '@/lib/utils';
+import { ExitIcon } from '@radix-ui/react-icons';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useAccount, useDisconnect, useEnsName } from 'wagmi';
 
 export const WalletConnect = () => {
-  return <ConnectButton />;
+  const { address } = useAccount();
+  const { data: ensName } = useEnsName({ address });
+  const { disconnect } = useDisconnect();
+
+  function handleDisconnect() {
+    disconnect();
+  }
+
+  return (
+    <ConnectButton.Custom>
+      {({ openConnectModal, account }) =>
+        account && address ? (
+          <span className="flex items-center gap-x-2">
+            <span>{ensName ?? abbreviateHash(address)}</span>
+
+            <button onClick={handleDisconnect} type="button" className="cursor-pointer">
+              <ExitIcon className="size-4" />
+            </button>
+          </span>
+        ) : (
+          <Button onClick={openConnectModal}>Connect Wallet</Button>
+        )
+      }
+    </ConnectButton.Custom>
+  );
 };
