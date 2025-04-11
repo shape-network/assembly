b3ecc1e5622f9437872cc2ebe87c49359076e0ae
William H <william.hermozo@protonmail.com>
2025-04-11 17:10:33 +0200

    f

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import { rainbowConfig } from '@/lib/web3';
+import { config } from '@/lib/web3';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode } from 'react';
+import { WagmiProvider } from 'wagmi';
 
 const queryClient = new QueryClient();
 
 export const Providers = ({ children }: { children: ReactNode }) => {
   return (
-    <WagmiProvider config={rainbowConfig}>
+    <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>{children}</RainbowKitProvider>
       </QueryClientProvider>
