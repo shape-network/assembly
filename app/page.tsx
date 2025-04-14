b51395f9cd15547bb7984446221c045d4ec65720
William H <william.hermozo@protonmail.com>
2025-04-14 10:23:58 +0200

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="grid min-h-screen place-items-center p-8 sm:p-20">
       <main className="flex flex-col items-center gap-8">
-        <h1 className="text-4xl font-bold">Assembly</h1>
-        <p className="text-lg">An otom-based item crafter</p>
+        <h1 className="text-primary text-4xl font-bold">ASSEMBLY</h1>
+        <p className="text-xl">An otom-based item crafter</p>
         <WalletConnect />
       </main>
     </div>
