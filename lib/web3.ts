e4b3bcaa743ed2b052f9dfb6fd0643261c249328
William H <william.hermozo@protonmail.com>
2025-04-14 11:06:24 +0200

    add alchemy client

--- lib/web3.ts
+++ lib/web3.ts
@@ -1,23 +1,28 @@
+import { config } from '@/lib/config';
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
+import { Alchemy, Network } from 'alchemy-sdk';
 import { http } from 'viem';
 import { shape, shapeSepolia } from 'viem/chains';
 
-const transports = {
-  [shape.id]: http(
-    `https://shape-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
-    { batch: true }
-  ),
-  [shapeSepolia.id]: http(
-    `https://shape-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
-    { batch: true }
-  ),
-};
-
-export const config = getDefaultConfig({
+export const wagmiConfig = getDefaultConfig({
   appName: 'Assembly',
+  ssr: true,
   projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
   chains: [shape, shapeSepolia],
-  transports,
-  ssr: true,
+  transports: {
+    [shape.id]: http(
+      `https://shape-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
+      { batch: true }
+    ),
+    [shapeSepolia.id]: http(
+      `https://shape-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
+      { batch: true }
+    ),
+  },
+});
+
+export const alchemy = new Alchemy({
+  apiKey: process.env.ALCHEMY_API_KEY,
+  network: config.chainId === shape.id ? Network.SHAPE_MAINNET : Network.SHAPE_SEPOLIA,
 });
