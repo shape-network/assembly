c5c3f684e45ec55366be1ac351f30f6e74499dfc
William H <william.hermozo@protonmail.com>
2025-04-14 12:21:45 +0200

    rpcClient

--- lib/web3.ts
+++ lib/web3.ts
@@ -2,7 +2,7 @@ import { config } from '@/lib/config';
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
 import { Alchemy, Network } from 'alchemy-sdk';
-import { http } from 'viem';
+import { createPublicClient, http } from 'viem';
 import { shape, shapeSepolia } from 'viem/chains';
 
 export const wagmiConfig = getDefaultConfig({
@@ -26,3 +26,14 @@ export const alchemy = new Alchemy({
   apiKey: process.env.ALCHEMY_API_KEY,
   network: config.chainId === shape.id ? Network.SHAPE_MAINNET : Network.SHAPE_SEPOLIA,
 });
+
+export function rpcClient() {
+  const chainId = config.chainId;
+  const chain = chainId === shape.id ? shape : shapeSepolia;
+  const rootUrl = chainId === shape.id ? 'shape-mainnet' : 'shape-sepolia';
+
+  return createPublicClient({
+    chain,
+    transport: http(`https://${rootUrl}.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`),
+  });
+}
