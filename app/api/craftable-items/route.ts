628986c7b3ce36c897ce4465aec43f0a5c6bd70f
William H <william.hermozo@protonmail.com>
2025-04-23 16:07:48 +0200

    add route

---
+++ app/api/craftable-items/route.ts
@@ -0,0 +1,73 @@
+import { itemsCoreContractAbi } from '@/generated';
+import { itemsCore } from '@/lib/addresses';
+import { rpcClient } from '@/lib/clients';
+import { config } from '@/lib/config';
+import { Item, Trait } from '@/lib/types';
+import { NextResponse } from 'next/server';
+import superjson from 'superjson';
+import { Address } from 'viem';
+
+async function getCraftItems(): Promise<Item[]> {
+  const rpc = rpcClient();
+
+  const nextItemId = await rpc.readContract({
+    abi: itemsCoreContractAbi,
+    address: itemsCore[config.chainId] as Address,
+    functionName: 'getNextItemId',
+    args: [],
+  });
+
+  const itemsLength = Number(nextItemId - BigInt(1));
+
+  const responses = await Promise.all(
+    Array.from({ length: itemsLength }, async (_, i) => {
+      const result = await rpc.readContract({
+        abi: itemsCoreContractAbi,
+        address: itemsCore[config.chainId] as Address,
+        functionName: 'items',
+        args: [BigInt(i + 1)],
+      });
+
+      return { result, id: BigInt(i + 1) };
+    })
+  );
+
+  return responses.map((r) => ({
+    id: r.id,
+    name: r.result[0],
+    description: r.result[1],
+    creator: r.result[2],
+    defaultImageUri: r.result[4],
+    traits: [],
+    blueprint: [],
+  }));
+}
+
+async function getTraitsForItem(itemId: bigint): Promise<Trait[]> {
+  const rpc = rpcClient();
+  const traits = await rpc.readContract({
+    abi: itemsCoreContractAbi,
+    address: itemsCore[config.chainId] as Address,
+    functionName: 'getTokenTraits',
+    args: [itemId],
+  });
+
+  return traits.map((t) => ({
+    name: t.typeName,
+    value: t.valueString ?? t.valueNumber.toString(),
+  }));
+}
+
+export async function GET() {
+  const items = await getCraftItems();
+
+  const itemsWithTraits = await Promise.all(
+    items.map(async (item) => {
+      const traits = await getTraitsForItem(item.id);
+      return { ...item, traits };
+    })
+  );
+
+  const serialized = superjson.stringify(itemsWithTraits);
+  return NextResponse.json(serialized);
+}
