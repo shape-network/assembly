2a05a161b860ad031fc53b10cd3eb11f31cc1c4d
William H <william.hermozo@protonmail.com>
2025-05-20 17:39:20 +0200

    item route

---
+++ app/api/item/route.ts
@@ -0,0 +1,86 @@
+import { assemblyCoreContractAbi } from '@/generated';
+import { assemblyCore } from '@/lib/addresses';
+import { rpcClient } from '@/lib/clients';
+import { config } from '@/lib/config';
+import { ItemType, OwnedItem } from '@/lib/types';
+import { NextResponse } from 'next/server';
+import superjson from 'superjson';
+
+import { z } from 'zod';
+
+const schema = z.object({
+  itemTokenId: z.coerce.bigint(),
+  itemId: z.coerce.bigint(),
+});
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const result = schema.safeParse(body);
+
+  if (!result.success) {
+    return NextResponse.json({ error: 'Invalid input' }, { status: 400 });
+  }
+
+  const { itemTokenId, itemId } = result.data;
+
+  const rpc = rpcClient();
+
+  try {
+    const [itemResponse, tierResponse, traitsResponse] = await rpc.multicall({
+      contracts: [
+        {
+          abi: assemblyCoreContractAbi,
+          address: assemblyCore[config.chainId],
+          functionName: 'getItemByItemId',
+          args: [itemId],
+        },
+        {
+          abi: assemblyCoreContractAbi,
+          address: assemblyCore[config.chainId],
+          functionName: 'nonFungibleTokenToTier',
+          args: [itemTokenId],
+        },
+        {
+          abi: assemblyCoreContractAbi,
+          address: assemblyCore[config.chainId],
+          functionName: 'getTokenTraits',
+          args: [itemTokenId],
+        },
+      ],
+      allowFailure: true,
+    });
+
+    const itemResult = itemResponse.status === 'success' ? itemResponse.result : null;
+    const tierResult = tierResponse.status === 'success' ? tierResponse.result : null;
+    const traitsResult = traitsResponse.status === 'success' ? traitsResponse.result : null;
+
+    if (itemResult === null) {
+      console.error(`An error occurred while fetching item ${itemId}:`, itemResponse.error);
+      return null;
+    }
+
+    const tier = Number(tierResult);
+    const usagesTrait = traitsResult?.find((trait) => trait.typeName === 'Usages Remaining');
+    const usagesRemaining = usagesTrait ? Number(usagesTrait.valueNumber) : null;
+
+    const item: OwnedItem = {
+      ...itemResult,
+      blueprint: [],
+      itemType: itemResult.itemType as ItemType,
+      tokenId: itemTokenId.toString(),
+      tier,
+      usagesRemaining,
+      initialTraits: traitsResult
+        ? traitsResult?.map((t) => ({
+            name: t.typeName,
+            value: t.valueString ?? t.valueNumber.toString(),
+          }))
+        : [],
+    };
+
+    return new NextResponse(superjson.stringify(item));
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ error: 'An error occurred while fetching item' }, { status: 500 });
+  }
+}
