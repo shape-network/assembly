29360ed3bb592699a2e2a9cbd0b5aa9fadddd75f
William H <william.hermozo@protonmail.com>
2025-05-21 17:58:55 +0200

    add create page

---
+++ apps/web/app/create/page.tsx
@@ -0,0 +1,9 @@
+import { ItemCreationForm } from '@/components/item-creation-form';
+
+export default function Create() {
+  return (
+    <main className="grid min-h-screen place-items-center">
+      <ItemCreationForm />
+    </main>
+  );
+}
--- apps/web/lib/paths.ts
+++ apps/web/lib/paths.ts
@@ -3,6 +3,7 @@ import { shape } from 'viem/chains';
 
 export const paths = {
   home: '/',
+  create: '/create',
   repo: 'https://github.com/shape-network/assembly',
   shape: 'https://shape.network',
   otom: 'https://otom.xyz',
