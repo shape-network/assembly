d56269cb8c32557d37eee9644c4db5ef8cdb9ab9
j6i <43463199+j6i@users.noreply.github.com>
2025-05-22 20:55:18 -0700

    update Medallion

--- packages/contracts/scripts/createFungibleItem.ts
+++ packages/contracts/scripts/createFungibleItem.ts
@@ -4,12 +4,12 @@ import { ethers } from 'hardhat';
 const OTOM_ITEMS_CORE_ADDRESS = '0x488B5bAEA1Eb28F48c279C9Ac4e3312790813C2e'; // Shape Sepolia
 
 const main = async () => {
-  // Grab the Medal template
-  const item = JSON.parse(fs.readFileSync('./scripts/templates/Medal.json').toString());
+  // Grab the Medallion template
+  const item = JSON.parse(fs.readFileSync('./scripts/templates/Medallion.json').toString());
 
   const core = await ethers.getContractAt('OtomItemsCore', OTOM_ITEMS_CORE_ADDRESS);
 
-  // Create the Medal
+  // Create the Medallion
   await core.createFungibleItem(
     item.name,
     item.description,
