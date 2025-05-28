b94563ae6e3b4fbafe1cae005b975a5380ed8e18
William H <william.hermozo@protonmail.com>
2025-05-28 11:37:23 +0200

    chain id

--- apps/web/lib/config.ts
+++ apps/web/lib/config.ts
@@ -2,6 +2,4 @@ import { shape, shapeSepolia } from 'viem/chains';
 
 export const config = {
   chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID) as typeof shape.id | typeof shapeSepolia.id,
-  selectedChain: process.env.NEXT_PUBLIC_CHAIN_ID === '1' ? shape : shapeSepolia,
-  isMainnet: process.env.NEXT_PUBLIC_CHAIN_ID === '1',
 };
