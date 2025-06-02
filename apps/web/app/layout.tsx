cf5b5fee1d1a49a4ebebfe2bce3c056a0a939153
William H <william.hermozo@protonmail.com>
2025-06-02 13:50:19 +0200

    move header in layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,12 @@
 import { Providers } from '@/components/providers';
+import { InlineLink } from '@/components/ui/link';
 import { Toaster } from '@/components/ui/sonner';
+import { WalletConnect } from '@/components/wallet-connect';
+import { paths } from '@/lib/paths';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import Link from 'next/link';
 import './globals.css';
 
 const geistSans = Geist({
@@ -30,8 +34,36 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} font-[family-name:var(--font-geist-mono)] antialiased`}
       >
-        <Providers>{children}</Providers>
-        <Toaster />
+        <div className="mx-auto grid min-h-screen max-w-7xl grid-rows-[auto_1fr] gap-4">
+          <Providers>
+            <header className="flex items-center justify-between p-5">
+              <div className="relative">
+                <h1 className="text-primary text-2xl font-semibold tracking-wide uppercase">
+                  <Link href={paths.home}>Assembly</Link>
+                </h1>
+                <span className="text-muted-foreground/50 absolute -bottom-8 left-0 text-sm whitespace-nowrap sm:-bottom-5">
+                  An otom-based item crafter
+                </span>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <InlineLink
+                  href={paths.otom}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="px-3"
+                >
+                  otom.xyz
+                </InlineLink>
+
+                <WalletConnect />
+              </div>
+            </header>
+
+            {children}
+            <Toaster />
+          </Providers>
+        </div>
       </body>
     </html>
   );
