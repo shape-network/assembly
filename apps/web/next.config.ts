87fbd5c48e44a2e4c8a66a22977f361d4f3dd88f
William H <william.hermozo@protonmail.com>
2025-06-04 17:46:13 +0200

    redirect

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -10,6 +10,15 @@ const nextConfig: NextConfig = {
       { hostname: '*' }, // only way to render images from any domain
     ],
   },
+  redirects: async () => {
+    return [
+      {
+        source: '/docs',
+        destination: 'https://docs.shape.network/building-on-shape/onchain-compatible/assembly',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
