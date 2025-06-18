0fcd0e4362ad0a28422a24514ac650044648120d
j6i <43463199+j6i@users.noreply.github.com>
2025-06-17 22:17:31 -0300

    handle mobile

--- apps/web/components/create-item-button.tsx
+++ apps/web/components/create-item-button.tsx
@@ -2,10 +2,12 @@
 
 import { Button } from '@/components/ui/button';
 import { paths } from '@/lib/paths';
+import { PencilRulerIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { usePostHog } from 'posthog-js/react';
 import { FC } from 'react';
 import { useAccount } from 'wagmi';
+import { useMediaQuery } from './hooks/useMediaQuery';
 
 export const CreateItemButton: FC = () => {
   const { address } = useAccount();
@@ -16,13 +18,14 @@ export const CreateItemButton: FC = () => {
     router.push(paths.create);
     posthog?.capture('click', { event: 'click_navbar', action: 'create_item' });
   };
+  const isMobile = useMediaQuery('sm');
 
   if (!address) return null;
 
   return (
     <div className="flex items-center gap-1">
-      <Button size="lg" onClick={onCreationClick}>
-        Create Assembly Item
+      <Button size={isMobile ? 'icon' : 'lg'} onClick={onCreationClick}>
+        {isMobile ? <PencilRulerIcon className="size-5" /> : 'Create Assembly Item'}
       </Button>
     </div>
   );
